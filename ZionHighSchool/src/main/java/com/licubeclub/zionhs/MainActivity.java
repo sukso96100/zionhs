4a473727781c7d6e2dca740af38ba24928b51609
sukso96100 <sukso96100@gmail.com>
2013-11-24 12:50:48 +0900

    Updated changes

--- ZionHighSchool/src/main/java/com/licubeclub/zionhs/MainActivity.java
+++ ZionHighSchool/src/main/java/com/licubeclub/zionhs/MainActivity.java
@@ -16,9 +16,13 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
 
 
 
 
-}
+
 }
