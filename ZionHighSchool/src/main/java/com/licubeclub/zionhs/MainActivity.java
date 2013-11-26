c776bb64a7f40e1be1cd90b0c0023a0395b07e51
sukso96100 <sukso96100@gmail.com>
2013-11-27 00:06:32 +0900

    Updated changes -Added Appinfo Activity -Updated Theme

--- ZionHighSchool/src/main/java/com/licubeclub/zionhs/MainActivity.java
+++ ZionHighSchool/src/main/java/com/licubeclub/zionhs/MainActivity.java
@@ -1,28 +1,38 @@
 package com.licubeclub.zionhs;
 
+
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.View.OnClickListener;
+
 
 public class MainActivity extends ActionBarActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        View notices = findViewById(R.id.notices);
+        View schoolinfo = findViewById(R.id.schoolinfo);
+        View appinfo = findViewById(R.id.info);
+        View meal = findViewById(R.id.meal);
+        View schedule = findViewById(R.id.schedule);
+        View schoolintro = findViewById(R.id.schoolintro);
+
+        appinfo.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Appinfo.class);
+                startActivity(intent);
+            }
+        });
 
     }
-
-
-
 
 
 }
