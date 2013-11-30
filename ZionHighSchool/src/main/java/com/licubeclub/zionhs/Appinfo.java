464d16261a4178adf213d2b7f3b53239412c9800
sukso96100 <sukso96100@gmail.com>
2013-11-30 11:29:31 +0900

    Updated changes -Updated Appinfo Activity

--- ZionHighSchool/src/main/java/com/licubeclub/zionhs/Appinfo.java
+++ ZionHighSchool/src/main/java/com/licubeclub/zionhs/Appinfo.java
@@ -1,15 +1,13 @@
 package com.licubeclub.zionhs;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
+import android.widget.TextView;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 public class Appinfo extends ActionBarActivity {
 
@@ -18,6 +16,27 @@ public class Appinfo extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appinfo);
 
+        //Get app version name from Manifest
+        String app_ver = null;
+        try {
+            app_ver = this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName;
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        //Set app version name text
+        TextView version = (TextView)findViewById(R.id.version);
+        version.setText("Version " + app_ver);
+
+        TextView src = (TextView)findViewById(R.id.src);
+        src.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent src = new Intent(Intent.ACTION_VIEW);
+                src.setData(Uri.parse("http://github.com/sukso96100/zionhs"));
+                startActivity(src);
+            }
+        });
 
     }
 
