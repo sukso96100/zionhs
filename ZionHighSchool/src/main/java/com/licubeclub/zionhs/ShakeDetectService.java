6dfc9bf78e388c40fa02d381b0929fe955e91313
sukso96100 <sukso96100@gmail.com>
2013-12-01 14:11:15 +0900

    Added ShakeDetectService

---
+++ ZionHighSchool/src/main/java/com/licubeclub/zionhs/ShakeDetectService.java
@@ -0,0 +1,14 @@
+package com.licubeclub.zionhs;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by youngbin on 13. 12. 1.
+ */
+public class ShakeDetectService extends Service {
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
