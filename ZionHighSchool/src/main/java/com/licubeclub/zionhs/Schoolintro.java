99d6abde41b4b7720b08bea45d3f6fce887b589e
sukso96100 <sukso96100@gmail.com>
2013-12-11 20:23:12 +0900

    Updated Changes -Removed ViewPagerIndicator -Added PagerTabStrip in Schoolintro Activity

--- ZionHighSchool/src/main/java/com/licubeclub/zionhs/Schoolintro.java
+++ ZionHighSchool/src/main/java/com/licubeclub/zionhs/Schoolintro.java
@@ -1,16 +1,92 @@
 package com.licubeclub.zionhs;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class Schoolintro extends ActionBarActivity {
+
+    private ViewPager mPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schoolintro);
 
-
+        mPager = (ViewPager)findViewById(R.id.pager);
+        mPager.setAdapter(new PagerAdapterClass(getApplicationContext()));
     }
+
+    @SuppressWarnings("unused")
+    private void setCurrentInflateItem(int type){
+        if(type==0){
+            mPager.setCurrentItem(0);
+        }else if(type==1){
+            mPager.setCurrentItem(1);
+        }
+        else{
+            mPager.setCurrentItem(5);
+        }
+    }
+    /**
+     * PagerAdapter
+     */
+    private class PagerAdapterClass extends PagerAdapter{
+
+        private LayoutInflater mInflater;
+        public PagerAdapterClass(Context c){
+            super();
+            mInflater = LayoutInflater.from(c);
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+
+        @Override
+        public Object instantiateItem(View pager, int position) {
+            View v = null;
+            if(position == 0){
+                v = mInflater.inflate(R.layout.schoolintro_0, null);
+            }
+            else if(position == 1){
+                v = mInflater.inflate(R.layout.schoolintro_1, null);
+            }
+            else{
+                v = mInflater.inflate(R.layout.schoolintro_2, null);
+            }
+            ((ViewPager)pager).addView(v, 0);
+            return v;
+        }
+        @Override
+        public void destroyItem(View pager, int position, Object view){
+            ((ViewPager)pager).removeView((View)view);
+        }
+
+        @Override
+        public boolean isViewFromObject(View pager, Object obj){
+            return pager == obj;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            if(position == 0){
+                return getString(R.string.school_symbol);
+            }
+            else if(position == 1){
+                return getString(R.string.goal_edu);
+            }
+            else{
+                return getString(R.string.fe_eb);
+            }
+        }
+    }
+
 
 }
