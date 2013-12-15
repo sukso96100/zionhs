1aaf70c01609d542ffc19bbb4e77cd40e484c208
sukso96100 <sukso96100@gmail.com>
2013-12-15 15:15:18 +0900

    Updated changes -new version name and new version code -Added License Comment blocks

--- ZionHighSchool/src/main/java/com/licubeclub/zionhs/ShakeDetectService.java
+++ ZionHighSchool/src/main/java/com/licubeclub/zionhs/ShakeDetectService.java
@@ -1,3 +1,21 @@
+/*
+ * Zion High School Application for Android
+ * Copyright (C) 2013 Youngbin Han<sukso96100@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.licubeclub.zionhs;
 
 import android.app.Notification;
@@ -13,9 +31,6 @@ import android.os.IBinder;
 import android.os.Vibrator;
 import android.util.Log;
 
-/**
- * Created by youngbin on 13. 12. 1.
- */
 public class ShakeDetectService extends Service {
 
     private SensorManager mSensorManager;
